b64f4d195286fd9467b94f443889a14c20679808
josecoborivas <josecoborivas@gmail.com>
2020-06-06 11:35:07 -0300

    grid

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -47,6 +47,11 @@ export class InicioPage implements OnInit {
       icon: 'car',
       name: 'Fab Buttons',
       redirectTo: '/fab'
+    },
+    {
+      icon: 'grid',
+      name: 'Grid - Rows',
+      redirectTo: '/grid'
     }
   ]
 
