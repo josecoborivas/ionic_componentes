a513fb640deef0033d38dd705f63c598fec232d5
josecoborivas <josecoborivas@gmail.com>
2020-06-08 18:12:58 -0300

    loading

---
+++ src/app/pages/loading/loading.page.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-loading',
+  templateUrl: './loading.page.html',
+  styleUrls: ['./loading.page.scss'],
+})
+export class LoadingPage implements OnInit {
+
+  loading: any;
+  constructor(private loadingCtrl: LoadingController) { }
+
+  ngOnInit() {
+    this.presentLoading('Espere');
+    setTimeout(() => {
+      this.loading.dismiss();
+    }, 1500);
+  }
+
+  async presentLoading(message: string) {
+     this.loading = await this.loadingCtrl.create({
+      cssClass: 'my-custom-class',
+      message
+      //duration: 2000
+    });
+    return this.loading.present();
+
+    
+  }
+
+}
