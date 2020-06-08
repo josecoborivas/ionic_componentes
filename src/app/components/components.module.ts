ea180c09e390a0f38c61563c7992dd0bcd436f11
josecoborivas <josecoborivas@gmail.com>
2020-06-08 19:49:19 -0300

    menu

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { IonicModule } from '@ionic/angular';
+import { MenuComponent } from './menu/menu.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
-  declarations: [HeaderComponent],
-  exports:[HeaderComponent],
+  declarations: [HeaderComponent, MenuComponent],
+  exports:[HeaderComponent, MenuComponent],
   imports: [
     CommonModule,
-    IonicModule
+    IonicModule,
+    RouterModule
   ]
 })
 export class ComponentsModule { }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Componente } from '../interfaces/intefaces';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,10 @@ export class DataService {
   
   getUsers() {
     return this.http.get('https://jsonplaceholder.typicode.com/users');
+  }
+
+  getMenuOptions(){
+    return this.http.get<Componente[]>('./assets/data/menu.json');
   }
 }
 
