a36cf1cd65b42bdd3acbe28413f7228e764a11ed
josecoborivas <josecoborivas@gmail.com>
2020-06-09 21:15:38 -0300

    Skeleton-text

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Componente } from '../interfaces/intefaces';
+import { delay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -22,7 +23,10 @@ export class DataService {
   }
 
   getSuperHeroes(){
-    return this.http.get('./assets/data/superheroes.json');
+    return this.http.get('./assets/data/superheroes.json')
+      .pipe(
+        delay(1500)
+      );
   }
 }
 
