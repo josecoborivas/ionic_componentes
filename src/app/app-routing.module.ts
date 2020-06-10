be6172e82f306d3ccacaf52e5abe12de06d6ed17
josecoborivas <josecoborivas@gmail.com>
2020-06-10 19:23:00 -0300

    slides

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -85,6 +85,10 @@ const routes: Routes = [
   {
     path: 'segment',
     loadChildren: () => import('./pages/segment/segment.module').then( m => m.SegmentPageModule)
+  },
+  {
+    path: 'slides',
+    loadChildren: () => import('./pages/slides/slides.module').then( m => m.SlidesPageModule)
   }
 
 
